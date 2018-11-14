2add6d8a627cbcf7fd1161b331072683b5463765
Richard Brooks <rickbrooks22@gmail.com>
2018-11-14 15:08:48 -0500

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Richard Brooks on 11/14/18.
+//  Copyright © 2018 Richard Brooks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
